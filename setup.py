953ea65c75e277f3ca0b0d31c45c3ba8fd718ed8
TheDen <deniskhoshaba@gmail.com>
2018-04-18 21:51:55 +1000

    update heuristic network discovery

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
   name='netmon',
-  version='0.1.0',
+  version='0.1.1',
   author='Denis Khshaba',
   author_email='deniskhoshaba@gmail.com',
   scripts=['netmon'],
