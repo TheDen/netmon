eb520423ca57a71facbb33da87df043b9614b165
TheDen <deniskhoshaba@gmail.com>
2018-05-29 02:01:53 +1000

    fmt with black

--- setup.py
+++ setup.py
@@ -2,25 +2,22 @@ from setuptools import setup
 import codecs
 
 try:
-  f = codecs.open('README.rst', encoding='utf-8')
-  long_description = f.read()
-  f.close()
+    f = codecs.open("README.rst", encoding="utf-8")
+    long_description = f.read()
+    f.close()
 except:
-  long_description = ''
+    long_description = ""
 
 setup(
-  name='netmon',
-  version='0.2.0',
-  author='Denis Khshaba',
-  author_email='deniskhoshaba@gmail.com',
-  scripts=['netmon'],
-  url = 'https://github.com/theden/netmon',
-  keywords = ['network', 'monitor', 'linux'],
-  license='GPL-2.0',
-  description='network monitor for linux',
-  long_description=long_description,
-  install_requires=[
-    'ascii_graph',
-    'cursor',
-  ]
+    name="netmon",
+    version="0.2.0",
+    author="Denis Khshaba",
+    author_email="deniskhoshaba@gmail.com",
+    scripts=["netmon"],
+    url="https://github.com/theden/netmon",
+    keywords=["network", "monitor", "linux"],
+    license="GPL-2.0",
+    description="network monitor for linux",
+    long_description=long_description,
+    install_requires=["ascii_graph", "cursor"],
 )
